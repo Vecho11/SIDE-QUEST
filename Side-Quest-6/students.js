d94e731cdb9a3ec84f4b04e24057b3ff1b80e8c7
Jj01001010 <jerichojj08@gmail.com>
2026-03-18 20:44:40 +0800

    REFACTORED  - Some Functions ADDED - Live Search ADJUSTMENT - UI (a little)

--- Side-Quest-6/students.js
+++ Side-Quest-6/students.js
@@ -1,78 +1,121 @@
 const students = [
     { student_id: 1, name: "Jericho", course: "BSIT" },
-    { student_id: 2, name: "JJ", course: "BSCS" },
+    { student_id: 2, name: "Jj", course: "BSCS" },
     { student_id: 3, name: "Merry", course: "BSBA" },
     { student_id: 4, name: "Anna", course: "BSIT" },
     { student_id: 5, name: "Mark", course: "BSCS" }
 ];
 
 const parentList = document.querySelector(".datas")
-const input = document.querySelector(".input")
-const findId = document.querySelector(".id-input")
-const button = document.querySelector(".event-button")
+const findStudentById = document.querySelector(".search-input")
+const searchButton = document.querySelector(".search-button")
+const showAllStudents = document.querySelector(".show-all-student ")
+const showCourseStatistics = document.querySelector(".show-course-stats")
+const nameInput = document.querySelector(".name-input")
+const nameSearch = document.querySelector(".find-name")
 
-const coursesCount = {}
-button.addEventListener("click", (e) => {
+function renderedList(text) {
+    const newList = document.createElement("li")
+    newList.textContent = text
+    parentList.appendChild(newList)
+}
+
+searchButton.addEventListener("click", (e) => {
     e.preventDefault()
-    studentFunc()
+    parentList.textContent = ""
+    searchStudent()
 })
 
-function studentFunc() {
-    const choiceValue = Number(input.value);
+showAllStudents.addEventListener("click", (e) => {
+    e.preventDefault()
     parentList.textContent = ""
+    displayAllStudents()
+})
 
+showCourseStatistics.addEventListener("click", (e) => {
+    e.preventDefault()
+    parentList.textContent = ""
+    displayCourseStats()
+})
 
-    if (isNaN(choiceValue) || choiceValue <= 0) {
-        parentList.textContent = "Invalid input"
+nameSearch.addEventListener("click", (e) => {
+    e.preventDefault()
+    parentList.textContent = ""
+    searchStudentName()
+})
+
+nameInput.addEventListener("keyup", (e) => {
+    parentList.textContent = ""
+    liveDataSearch()
+})
+
+function searchStudent() {
+    const searchID = Number(findStudentById.value)
+    findStudentById.value = ""
+
+    if (isNaN(searchID) || searchID <= 0) {
+        parentList.textContent = "Invalid ID"
         return;
     }
-    if (choiceValue === 1) {
-        for (let student of students) {
-            const newList = document.createElement("li")
-            newList.textContent = `${student.student_id} ${student.name} - ${student.course}`
-            parentList.appendChild(newList)
-        }
-        input.value = ""
-    }
-    else if (choiceValue === 2) {
-        const searchID = Number(findId.value)
-        let found = false
+    const student = students.find(id => id.student_id === searchID)
 
-        for (let student of students) {
-            if (searchID === student.student_id) {
-                const newList = document.createElement("li")
-                newList.textContent = `${student.student_id} - ${student.name} : ${student.course}`
-                parentList.appendChild(newList)
-                found = true
-            }
-            input.value = ""
-        }
-        if (!found) {
-            const newList = document.createElement("li")
-            newList.textContent = "Student ID not found."
-            parentList.appendChild(newList)
-            input.value = ""
-            findId.value = ""
-        }
-    } else if (choiceValue === 3) {
-
-        for (let student of students) {
-            if (coursesCount[student.course]) {
-                coursesCount[student.course] += 1
-            } else {
-                coursesCount[student.course] = 1
-            }
-        }
-        for (let course in coursesCount) {
-            const newList = document.createElement("li")
-            newList.textContent = `${course}: ${coursesCount[course]}`
-            parentList.appendChild(newList)
-        }
-        input.value = ""
+    if(student){
+        renderedList(`${student.student_id} - ${student.name} : ${student.course}`)
+    } else {
+        renderedList("Student ID not found.")
     }
-    else if (choiceValue === 4) {
-        console.log("Program ended.")
-        parentList.textContent = "Program ended."
+}
+
+function searchStudentName(){
+    const findName = nameInput.value.toLowerCase().trim()
+    nameInput.value = ""
+
+    if(findName === null){
+        parentList.textContent = "Invalid Name!"
+        return;
+    }
+
+    const student = students.find(s => s.name.toLowerCase() === findName)
+
+    if(student){
+        renderedList(`${student.student_id} - ${student.name} : ${student.course}`)
+    } else {
+        renderedList("Student name not found.")
+    }
+
+}
+
+function liveDataSearch(){
+    const findName = nameInput.value.toLowerCase().trim()
+    parentList.textContent = ""
+
+    const student = students.filter(s => 
+        s.name.toLowerCase().includes(findName))
+
+    student.forEach((s) => {
+        if(s){
+            renderedList(`${s.student_id} - ${s.name} : ${s.course}`)
+        }
+   })
+}
+
+function displayAllStudents() {
+    for (let student of students) {
+        renderedList(`${student.student_id} ${student.name} - ${student.course}`)
+    }
+}
+
+function displayCourseStats() {
+    const coursesCount = {}
+    for (let student of students) {
+        if (coursesCount[student.course]) {
+            coursesCount[student.course] += 1
+        } else {
+            coursesCount[student.course] = 1
+        }
+    }
+    for (let course in coursesCount) {
+        renderedList(`${course}: ${coursesCount[course]}`)
     }
 }
 

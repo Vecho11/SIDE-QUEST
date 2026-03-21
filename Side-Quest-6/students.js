01e4e2d4341670d8615a35a304d2a8d2fd8953e1
Jj01001010 <jerichojj08@gmail.com>
2026-03-21 21:14:35 +0800

    SQ6 - Commit

--- Side-Quest-6/students.js
+++ Side-Quest-6/students.js
@@ -1,23 +1,148 @@
-const students = [
-    { student_id: 1, name: "Jericho", course: "BSIT" },
-    { student_id: 2, name: "Jj", course: "BSCS" },
-    { student_id: 3, name: "Merry", course: "BSBA" },
-    { student_id: 4, name: "Anna", course: "BSIT" },
-    { student_id: 5, name: "Mark", course: "BSCS" }
-];
-
 const parentList = document.querySelector(".datas")
 const findStudentById = document.querySelector(".search-input")
 const searchButton = document.querySelector(".search-button")
-const showAllStudents = document.querySelector(".show-all-student ")
+const showAllStudents = document.querySelector(".show-all-student")
 const showCourseStatistics = document.querySelector(".show-course-stats")
 const nameInput = document.querySelector(".name-input")
 const nameSearch = document.querySelector(".find-name")
+const addStudentInput = document.querySelector(".add-student-input")
+const addCourseInput = document.querySelector(".add-course-input")
+const removeInput = document.querySelector(".remove-input")
+const removeStudentBtn = document.querySelector(".remove-student-btn")
+const addStudentBtn = document.querySelector(".add-student-btn")
+
+let students = [];
+let id = 1;
+
+function addNewStudent(){
+    const name = addStudentInput.value.trim()
+    const course = addCourseInput.value.trim()
+
+    if(course === "" || name === ""){
+        renderedList("Invalid Course / Name.")
+        return;
+    }
+    students.push({
+        student_id: id++,
+        name, 
+        course
+    });
+
+    addStudentInput.value = ""
+    addCourseInput.value = ""   
+}
+
+function removeStudent(){
+    const idToFound = Number(removeInput.value.trim());
+    
+    if(isNaN(idToFound)){
+        renderedList("Invalid ID, Student not found.")
+        return;
+    }
+
+    const index = students.findIndex(s => s.student_id === idToFound)
+    
+    if(index !== -1){
+        students.splice(index, 1);
+        removeInput.value = ""
+    }else{
+        renderedList("Invalid ID, Student not found.")
+        removeInput.value = ""
+    }
+}
 
 function renderedList(text) {
     const newList = document.createElement("li")
     newList.textContent = text
     parentList.appendChild(newList)
+}
+
+function searchStudent() {
+    const searchID = Number(findStudentById.value)
+    findStudentById.value = ""
+
+    if (isNaN(searchID) || searchID <= 0) {
+        renderedList("Invalid ID.")
+        return;
+    }
+    const student = students.find(id => id.student_id === searchID)
+
+    if(student){
+        renderedList(renderedData(student))
+    } else {
+        renderedList("Student ID not found.")
+    }
+}
+
+function renderedData(student){
+     return `${student.student_id} - ${student.name} : ${student.course}`
+}
+
+function searchStudentName(){
+    const findName = nameInput.value.toLowerCase().trim()
+    nameInput.value = ""
+
+
+    if(findName === ""){
+        renderedList("Invalid name.")
+        return;
+    }
+
+    const student = students.find(s => s.name.toLowerCase() === findName)
+
+    if(student){
+        renderedList(renderedData(student))
+    } else {
+        renderedList("Student name not found.")
+    }
+
+}
+
+function liveDataSearch(){
+    const findName = nameInput.value.toLowerCase().trim()
+    parentList.textContent = ""
+
+    const student = students.filter(s => 
+        s.name.toLowerCase().includes(findName))
+
+    student.forEach((s) => {
+        if(s){
+            renderedList(renderedData(s))
+        }
+   })
+}
+
+function displayAllStudents() {
+    parentList.textContent = ""
+
+    if(students.length <= 0){
+        renderedList("No student in the record.")
+        return;
+    }
+
+    for (let student of students) {
+        renderedList(renderedData(student))
+    }
+}
+
+function displayCourseStats() {
+    const coursesCount = {}
+
+    if(students.length <= 0){
+        renderedList("No course in the record.")
+        return;
+    }
+
+    for (let student of students) {
+        if (coursesCount[student.course]) {
+            coursesCount[student.course] += 1
+        } else {
+            coursesCount[student.course] = 1
+        }
+    }
+    for (let course in coursesCount) {
+        renderedList(`${course}: ${coursesCount[course]}`)
+    }
 }
 
 searchButton.addEventListener("click", (e) => {
@@ -49,81 +174,17 @@ nameInput.addEventListener("keyup", (e) => {
     liveDataSearch()
 })
 
-function searchStudent() {
-    const searchID = Number(findStudentById.value)
-    findStudentById.value = ""
-
-    if (isNaN(searchID) || searchID <= 0) {
-        parentList.textContent = "Invalid ID"
-        return;
-    }
-    const student = students.find(id => id.student_id === searchID)
-
-    if(student){
-        renderedList(`${student.student_id} - ${student.name} : ${student.course}`)
-    } else {
-        renderedList("Student ID not found.")
-    }
-}
-
-function searchStudentName(){
-    const findName = nameInput.value.toLowerCase().trim()
-    nameInput.value = ""
-
-    if(findName === null){
-        parentList.textContent = "Invalid Name!"
-        return;
-    }
-
-    const student = students.find(s => s.name.toLowerCase() === findName)
-
-    if(student){
-        renderedList(`${student.student_id} - ${student.name} : ${student.course}`)
-    } else {
-        renderedList("Student name not found.")
-    }
-
-}
-
-function liveDataSearch(){
-    const findName = nameInput.value.toLowerCase().trim()
+addStudentBtn.addEventListener("click", (e) => {
+    e.preventDefault()
     parentList.textContent = ""
+    addNewStudent()
+})
 
-    const student = students.filter(s => 
-        s.name.toLowerCase().includes(findName))
-
-    student.forEach((s) => {
-        if(s){
-            renderedList(`${s.student_id} - ${s.name} : ${s.course}`)
-        }
-   })
-}
-
-function displayAllStudents() {
-    for (let student of students) {
-        renderedList(`${student.student_id} ${student.name} - ${student.course}`)
-    }
-}
-
-function displayCourseStats() {
-    const coursesCount = {}
-    for (let student of students) {
-        if (coursesCount[student.course]) {
-            coursesCount[student.course] += 1
-        } else {
-            coursesCount[student.course] = 1
-        }
-    }
-    for (let course in coursesCount) {
-        renderedList(`${course}: ${coursesCount[course]}`)
-    }
-}
-
-
-
-
-
-
+removeStudentBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    parentList.textContent = ""
+    removeStudent()
+})
 
 
 
